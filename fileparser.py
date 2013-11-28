55e24b5b608a838ff8e1214b6c78c61c05d9db62
pspieker <pspieker@users.noreply.github.com>
2013-11-27 22:06:21 -0800

    Updated fileparser.py
    Now users are allowed to enter the folder to be parsed and the date before which files will be deleted in the program rather than modify the code itself.

--- fileparser.py
+++ fileparser.py
@@ -5,32 +5,33 @@ import os
 #and whether they should be deleted.
 from datetime import datetime
 
-#This is the start of the path.
-#Everything in this directory will be parsed
-toplevel = input("Please enter the path of the folder where the parsing will begin."
-                 "Remember that everything in that folder which is older than the date specified will be deleted."
 
-month, day ,year = input("Please enter the date which all files before "
-                         "will be deleted in the format: "
-                         "month, day, year. Please enter all as nunbers.").split() 
 #Defining functions for the rest of the program
 
 #Given a filepath, this will check to creaton date of the file
 #and delete it if it wasn't created after a certain date
-def datecheck(filepath):
+def datecheck(filepath, month, day, year):
      secs = os.path.getmtime(filepath)
      filedatetime = datetime.fromtimestamp(secs)
-     if filedatetime < datetime(2012,1,1):
+     if filedatetime < datetime(year,month,day):
           print(filedatetime)
           os.remove(filepath)
           print("The file,", file, " has been deleted")
 
 #Setting up the main program loop
 def main():
+     #This is the start of the path.
+     #Everything in this directory will be parsed
+     toplevel = input("Please enter the path of the folder where the parsing will begin."
+                 "Remember that everything in that folder which has not been modified before the date specified will be deleted."
+     print("Next, you will need to set a date so that the program can delete all files in the folder before it.")
+     year = input("Please enter a year: ")
+     month = input("Please enter a month (1-12): ")
+     day = input("Please enter a day: ")            
      for root, dirs, files in os.walk(toplevel):
           for file in files:
                path = os.path.join(root, file)
-               datecheck(path)
+               datecheck(path, month, day, year)
 
 #Calling main() to begin the program
 main()
